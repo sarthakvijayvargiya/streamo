a40ce30a006098670a2c5aeb189c3fd79e6890ec
sarthakvijayvargiya <vsarthakvijay@gmail.com>
2025-05-21 23:32:29 +0530

    implemented save video logic

---
+++ spring-streamo-backend/src/main/java/com/streamo/app/spring_streamo_backend/controllers/VideoController.java
@@ -0,0 +1,9 @@
+package com.streamo.app.spring_streamo_backend.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class VideoController {
+}
--- spring-streamo-backend/src/main/java/com/streamo/app/spring_streamo_backend/services/VideoServiceImpl.java
+++ spring-streamo-backend/src/main/java/com/streamo/app/spring_streamo_backend/services/VideoServiceImpl.java
@@ -1,17 +1,49 @@
 package com.streamo.app.spring_streamo_backend.services;
 
 import com.streamo.app.spring_streamo_backend.entity.Streamo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Service
-public class VideoServiceImpl implements VideoService{
+public class VideoServiceImpl implements VideoService {
 
+    @Value("${files.video}")
+    String DIR;
 
     @Override
     public Streamo saveVideo(Streamo video, MultipartFile file) {
+//       Folder Path : create
+        try {
+
+//        Get Original File Name
+            String fileName = file.getOriginalFilename();
+            String contentType = file.getContentType();
+            InputStream inputStream = file.getInputStream();
+
+
+//        Fodder path with file name
+            String cleanFileName = StringUtils.cleanPath(fileName);
+            String cleanFolder = StringUtils.cleanPath(DIR);
+
+            Path path = Paths.get(cleanFolder , cleanFileName);
+
+            System.out.println(path);
+//        Copy file to the folder
+
+//        video metadata
+
+//        save metadata to DB
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
