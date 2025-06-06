eb0cd6f08474a65e2d4a39a2ecf42a6dc7ed672b
sarthakvijayvargiya <vsarthakvijay@gmail.com>
2025-06-07 01:29:55 +0530

    added logo

--- streamo-ui/src/components/navbar/Navbar.tsx
+++ streamo-ui/src/components/navbar/Navbar.tsx
@@ -1,12 +1,15 @@
-import { UploadCloud, Video } from "lucide-react";
+import { UploadCloud } from "lucide-react";
 import { Link } from "react-router-dom";
+import streamo from "../../assets/images/streamo-logo.png";
 
 const Navbar = () => {
   return (
-    <nav className="w-full bg-[#11182C] text-white px-6 py-4 shadow-md">
+    <nav className="w-full bg-[#11182C] text-white px-6 py-2 shadow-md">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         {/* Logo / App Name */}
-        <div className="text-xl font-bold text-white tracking-wide">MyApp</div>
+        <div>
+          <img src={streamo} alt="Streamo Logo" className="h-12 w-12" />
+        </div>
 
         {/* Navigation Buttons */}
         <div className="flex space-x-4">
@@ -16,12 +19,12 @@ const Navbar = () => {
               <Link to="/">Upload</Link>
             </span>
           </button>
-          <button className="flex items-center space-x-1 text-xl hover:text-[#7965C1] transition">
+          {/* <button className="flex items-center space-x-1 text-xl hover:text-[#7965C1] transition">
             <Video className="w-5 h-5" />
             <span className="hidden sm:inline">
               <Link to="/video/:id">Videos</Link>
             </span>
-          </button>
+          </button> */}
         </div>
       </div>
     </nav>
